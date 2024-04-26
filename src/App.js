ac6f6c50966b7e23e65087d2ca7fca22898caeaa
Jin Minho <105831006+mlnho@users.noreply.github.com>
2024-04-26 18:22:51 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          깃업로드됐지롱
         </p>
         <a
           className="App-link"
