03f210203aa219d3d5831baa245c9341455efb03
Jin Minho <105831006+mlnho@users.noreply.github.com>
2024-04-29 16:11:36 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ecs...ecr...제발 
+          블루그린되낭요?
         </p>
         <a
           className="App-link"
