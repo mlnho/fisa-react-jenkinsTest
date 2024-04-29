73bb5132e25c16e518442814deca09becf7aa510
Jin Minho <105831006+mlnho@users.noreply.github.com>
2024-04-29 11:19:33 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ㅌㅅㅇㅁㄴㅇㄴㅁㅇㄴㅁㅇㄴㅁㅇㄴㅁㅇㅁㄴㅇ
+          블루그린되는거냐??
         </p>
         <a
           className="App-link"
