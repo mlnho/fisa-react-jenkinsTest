abc05fa37b86df8b1ff3ab603c9688ba6d638a5d
Jin Minho <105831006+mlnho@users.noreply.github.com>
2024-04-30 11:16:13 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          젠킨스로 테스트해봅니다 그냥 블루그린
+          블루그린 ㅍ배포중,,,,
         </p>
         <a
           className="App-link"
